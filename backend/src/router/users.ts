4872161f4e5cb831690f1d07e939df031607cd36
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-11 20:56:10 +0200

    added isAuthenticated middeware to users api to prevent exposure if not authenticated

--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middlewares';
 
 const router = express.Router();
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
 }
