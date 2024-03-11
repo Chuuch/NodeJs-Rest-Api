8b57cfba04d24c79fbbaa353e38c874699bab386
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-11 20:09:28 +0200

    added hex argument to digest method

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -7,5 +7,5 @@ export const random = () => crypto
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/'))
     .update(`${process.env.SECRET}`)
-    .digest();
+    .digest('hex');
 }
