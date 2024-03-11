25dbffd4a8739f2cc72f213646742f07c0be4c67
Chuuch <daniel.chuchulev96@gmail.com>
2024-03-11 21:07:34 +0200

    added delete user route to router

--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers } from '../controllers/users';
 import { isAuthenticated } from '../middlewares';
 
 const router = express.Router();
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
+    router.delete('/users/:id', deleteUser);
 }
